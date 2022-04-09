b518167cf0db6bc12beb9a87c9a3bd88240b3b2f
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-04-09 21:20:11 +0300

    Variable for easier change of api

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -1,1 +1,5 @@
-export const API_PATH = "https://localhost:5001/";
+const isDev = true;
+
+export const API_PATH = isDev
+  ? "https://localhost:5001/"
+  : "https://warehouse-magnus.azurewebsites.net/";

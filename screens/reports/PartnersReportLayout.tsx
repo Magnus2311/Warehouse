8ed932cc87caed3d4721f78e7f7bb6cc048ca151
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-04-15 22:29:11 +0300

    PartnersLayout component

---
+++ screens/reports/PartnersReportLayout.tsx
@@ -0,0 +1,25 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { Avatar, Card } from "react-native-paper";
+
+const LeftContent = (props: any) => <Avatar.Icon {...props} icon="briefcase" />;
+
+const PartnersReportLayout = () => {
+  const navigator = useNavigation();
+
+  return (
+    <Card
+      onPress={() => {
+        navigator.navigate("Reports");
+      }}
+    >
+      <Card.Title
+        title="Справка Партньори"
+        subtitle="Всички продажби групирани по партньор"
+        left={LeftContent}
+      />
+    </Card>
+  );
+};
+
+export default PartnersReportLayout;

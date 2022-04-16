754f5f6922dbfa4a37b940ed9ce4bb7649bf7633
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-04-16 12:09:22 +0300

    Working on partners report

--- screens/reports/PartnersReport.tsx
+++ screens/reports/PartnersReport.tsx
@@ -1,7 +1,18 @@
+import { connect } from "react-redux";
 import { Text } from "../../components/Themed";
+
+const mapDispatchToProps = () => {
+    return {
+        onLoadPartnersReport: ()
+    }
+}
+
+interface Props {
+
+}
 
 const PartnersReport = () => {
   return <Text>adljjiwdaiwdiawdilj</Text>;
 };
 
-export default PartnersReport;
+export default connect(null, mapDispatchToProps)(PartnersReport);

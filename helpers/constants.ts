4bff8d4725529afba04a9ed8632820d33d248b64
Iavor Orlyov <iavor_orlyov@yahoo.com>
2022-05-26 13:59:30 +0300

    Added missings urls

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -4,9 +4,9 @@ enum Environments {
   Production,
 }
 
-const warehouseEnvironment = Environments.Dev;
-const ssoEnvironment = Environments.Dev;
-const warehouseWebEnvironment = Environments.Dev;
+const warehouseEnvironment = Environments.Production;
+const ssoEnvironment = Environments.Production;
+const warehouseWebEnvironment = Environments.Production;
 
 const warehouseApi = (environment: Environments) => {
   switch (environment) {
@@ -24,9 +24,9 @@ const ssoApi = (environment: Environments) => {
     case Environments.Dev:
       return "https://localhost:7206/api";
     case Environments.Testing:
-      return "";
+      return "https://magnus-sso.azurewebsites.net/api";
     case Environments.Production:
-      return "";
+      return "https://magnus-sso.azurewebsites.net/api";
   }
 };
 
@@ -35,9 +35,9 @@ const warehouseWebAddress = (environment: Environments) => {
     case Environments.Dev:
       return "http://192.168.0.129:19006";
     case Environments.Testing:
-      return "";
+      return "https://magnus2311.github.io/Warehouse";
     case Environments.Production:
-      return "";
+      return "https://magnus2311.github.io/Warehouse";
   }
 };
 
